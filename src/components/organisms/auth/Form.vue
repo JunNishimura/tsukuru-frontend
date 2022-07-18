907aa8c62d27f52db6597812db3713136f6dec91
JunNishimura <n.junjun0303@gmail.com>
2022-07-18 20:38:02 +0900

    add formData object

--- src/components/organisms/auth/Form.vue
+++ src/components/organisms/auth/Form.vue
@@ -3,40 +3,47 @@
     <div class="authForm-title">
       {{ formTitle }}
     </div>
-    <form>
+    <form @submit.prevent="submitForm">
       <div v-if="isSignup" class="authForm-group">
         <label for="username">
           ユーザー名
         </label>
-        <input id="username" type="text" placeholder="つくる 太郎">
+        <input id="username" v-model="formData.username" type="text" placeholder="つくる 太郎">
       </div>
       <div class="authForm-group">
         <label for="e-mail">
           メールアドレス
         </label>
-        <input id="e-mail" type="text" placeholder="tsukuru.taro@example.com">
+        <input id="e-mail" v-model="formData.email" type="text" placeholder="tsukuru.taro@example.com">
       </div>
       <div class="authForm-group">
         <label for="password-1">
           パスワード
         </label>
-        <input id="password-1" type="text">
+        <input id="password-1" v-model="formData.password1" type="password">
       </div>
       <div v-if="isSignup" class="authForm-group">
         <label for="password-2">
           パスワード（再確認）
         </label>
-        <input id="password-2" type="text">
+        <input id="password-2" v-model="formData.password2" type="password">
       </div>
       <div class="authForm-group authForm-button">
-        <button>送信</button>
+        <input type="submit" value="送信">
       </div>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, useRoute } from '@nuxtjs/composition-api'
+import { defineComponent, useRoute, reactive } from '@nuxtjs/composition-api'
+
+type FormData = {
+  username: string
+  email: string
+  password1: string
+  password2: string
+}
 
 export default defineComponent({
   setup() {
@@ -44,10 +51,22 @@ export default defineComponent({
 
     const formTitle = route.value.name === 'signup' ? '新規登録' : 'ログイン'
     const isSignup = route.value.name === 'signup'
+    const formData = reactive<FormData>({
+      username: '',
+      email: '',
+      password1: '',
+      password2: ''
+    })
+
+    const submitForm = () => {
+      console.log(formData)
+    }
 
     return {
       formTitle,
-      isSignup
+      isSignup,
+      formData,
+      submitForm
     }
   }
 })
@@ -83,7 +102,7 @@ export default defineComponent({
       padding-left: 8px;
       background-color: $colorLightBlue;
     }
-    button {
+    input[type="submit"] {
       color: #ffffff;
       background-color: $colorGreen;
       cursor: pointer;
