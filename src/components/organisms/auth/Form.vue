7f03df45cefa2eb14d2adc0166cbaba1039bf3a8
JunNishimura <n.junjun0303@gmail.com>
2022-07-19 22:33:23 +0900

    add basic validation #9

--- src/components/organisms/auth/Form.vue
+++ src/components/organisms/auth/Form.vue
@@ -8,25 +8,25 @@
         <label for="username">
           ユーザー名
         </label>
-        <input id="username" v-model="formData.username" type="text" placeholder="つくる 太郎">
+        <input id="username" v-model="username.value" type="text" placeholder="つくる 太郎" @blur="() => username.isValid = true">
       </div>
       <div class="authForm-group">
         <label for="e-mail">
           メールアドレス
         </label>
-        <input id="e-mail" v-model="formData.email" type="text" placeholder="tsukuru.taro@example.com">
+        <input id="e-mail" v-model="email.value" type="text" placeholder="tsukuru.taro@example.com" @blur="() => email.isValid = true">
       </div>
       <div class="authForm-group">
-        <label for="password-1">
+        <label for="password">
           パスワード
         </label>
-        <input id="password-1" v-model="formData.password1" type="password">
+        <input id="password" v-model="password.value" type="password" @blur="() => password.isValid = true">
       </div>
       <div v-if="isSignup" class="authForm-group">
-        <label for="password-2">
+        <label for="passwordConfirm">
           パスワード（再確認）
         </label>
-        <input id="password-2" v-model="formData.password2" type="password">
+        <input id="passwordConfirm" v-model="passwordConfirm.value" type="password" @blur="() => passwordConfirm.isValid = true">
       </div>
       <div class="authForm-group authForm-button">
         <input type="submit" value="送信">
@@ -37,12 +37,11 @@
 
 <script lang="ts">
 import { defineComponent, useRoute, reactive } from '@nuxtjs/composition-api'
+import { required, email as emailValidator, password as passwordValidator } from '~/helpers/validation'
 
-type FormData = {
-  username: string
-  email: string
-  password1: string
-  password2: string
+type InputData = {
+  value: string
+  isValid: boolean
 }
 
 export default defineComponent({
@@ -51,21 +50,56 @@ export default defineComponent({
 
     const formTitle = route.value.name === 'signup' ? '新規登録' : 'ログイン'
     const isSignup = route.value.name === 'signup'
-    const formData = reactive<FormData>({
-      username: '',
-      email: '',
-      password1: '',
-      password2: ''
+    const username = reactive<InputData>({
+      value: '',
+      isValid: false
+    })
+    const email = reactive<InputData>({
+      value: '',
+      isValid: false
+    })
+    const password = reactive<InputData>({
+      value: '',
+      isValid: false
+    })
+    const passwordConfirm = reactive<InputData>({
+      value: '',
+      isValid: false
     })
 
-    const submitForm = () => {
-      console.log(formData)
+    const isPasswordMatched = password.value === passwordConfirm.value
+    const validateForm = (): void => {
+      if (!required(username.value)) {
+        username.isValid = false
+      }
+      if (!(required(email.value) && emailValidator(email.value))) {
+        email.isValid = false
+      }
+      if (!(required(password.value) && passwordValidator(password.value))) {
+        password.isValid = false
+      }
+      if (!(required(passwordConfirm.value) && passwordValidator(passwordConfirm.value) && isPasswordMatched )) {
+        passwordConfirm.isValid = false
+      }
+    }
+
+    const submitForm = (): void => {
+      validateForm()
+
+      if (username.isValid && email.isValid && password.isValid && passwordConfirm.isValid) {
+        console.log('success')
+      } else {
+        console.log('fail')
+      }
     }
 
     return {
       formTitle,
       isSignup,
-      formData,
+      username,
+      email,
+      password,
+      passwordConfirm,
       submitForm
     }
   }
