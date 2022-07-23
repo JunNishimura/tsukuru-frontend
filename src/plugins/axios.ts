a941e59f77f8bb3bf35df904932e5c371ca23413
JunNishimura <n.junjun0303@gmail.com>
2022-07-24 01:57:42 +0900

    add axios plugin #11

---
+++ src/plugins/axios.ts
@@ -0,0 +1,13 @@
+// import { Context } from '@nuxt/types'
+
+// export default function({ $axios } : Context) {
+//   $axios.onRequest((request) => {
+//     console.log(request)
+//   })
+//   $axios.onResponse((response) => {
+//     console.log(response)
+//   })
+//   $axios.onError((e) => {
+//     console.log(e.response)
+//   })
+// }
