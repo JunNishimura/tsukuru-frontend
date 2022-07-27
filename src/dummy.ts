e7e17be4f229aa7ea68ec11b4484ac701222cfa2
JunNishimura <n.junjun0303@gmail.com>
2022-07-28 00:16:48 +0900

    add dummy data

---
+++ src/dummy.ts
@@ -0,0 +1,29 @@
+export const userSample = {
+  id: 1,
+  username: 'jun',
+  email: 'jun@example.com',
+  password: 'password',
+  profile_image: '',
+  organization: 'ツクール大学',
+  occupation: '学生',
+  profile_word: '機械学習に興味があります'
+}
+
+export const ideasSample = [{
+  id: 1,
+  user_id: 1,
+  title: '学生のためのマッチングアプリを作りたい',
+  description: '大学生向けのマッチングアプリ。同じ大学で出会ったことのない人が気軽に出会えるようなアプリを作りたい。',
+  post_date: '20220724'
+}]
+
+export const tagsSample = [
+  {
+    id: 1,
+    name: 'nuxt.js'
+  },
+  {
+    id: 2,
+    name: 'Ruby on Rails'
+  }
+]
