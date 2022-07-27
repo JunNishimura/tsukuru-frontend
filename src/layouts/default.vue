2bb0c5aeec42aca4cb69c0075fc3e740e0e597bf
JunNishimura <n.junjun0303@gmail.com>
2022-07-28 00:16:26 +0900

    update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,14 +4,15 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-size: 24px;
+  font-size: $fontSize-2;
+  color: $colorDark;
   font-family: "M PLUS Rounded 1c", "ヒラギノ丸ゴ ProN W4", "Yu Gothic UI", "Yu Gothic", sans-serif;
 }
 </style>
