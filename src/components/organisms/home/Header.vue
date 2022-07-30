8e087b0a7de04e4922268c6ac0a8e5d1235f3779
JunNishimura <n.junjun0303@gmail.com>
2022-07-31 02:10:49 +0900

    add home header #15

---
+++ src/components/organisms/home/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="homeHeader">
+    <div class="homeHeader-container">
+      <div class="homeHeader-container__item">
+        タイムライン
+      </div>
+      <div class="homeHeader-container__item">
+        トレンド
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.homeHeader {
+  width: 100%;
+  background-color: $colorDarkBlue;
+  color: #ffffff;
+  &-container {
+    margin: 0 auto;
+    width: 60%;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    &__item + &__item {
+      padding-left: 24px;
+    }
+  }
+}
+</style>
